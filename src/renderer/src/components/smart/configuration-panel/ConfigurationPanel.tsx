e533675c4ca5ca0471d5b96413db4d35c6308cf4
Daniel de Fuenmayor <fmixagent@gmail.com>
2026-03-23 15:06:13 +0700

    add select field component with react-select

--- src/renderer/src/components/smart/configuration-panel/ConfigurationPanel.tsx
+++ src/renderer/src/components/smart/configuration-panel/ConfigurationPanel.tsx
@@ -4,6 +4,8 @@ import ListSelector, { ListItem } from '../list-selector/list-selector';
 import { PRELOADED_SESSIONs, Session } from '@renderer/models/session';
 import { TimeStretch, UserConfiguration } from '@renderer/models/userConfiguration';
 import { X } from 'react-bootstrap-icons';
+import SelectField, { SelectFieldOption } from '../select-field/select-field';
+import CreatableSelectField from '../creatable-select-field/creatable-select-field';
 
 interface ConfigurationPanelProps {
   userConfiguration: UserConfiguration;
@@ -54,8 +56,21 @@ const ConfigurationPanel: React.FC<ConfigurationPanelProps> = ({
     onChange?.(newConfiguration);
   }
 
+  const [selectedSessionOption, setSelectedSessionOption] = React.useState<SelectFieldOption<Session> | undefined>(
+    userConfiguration?.sessionSelected
+      ? {
+          label: userConfiguration.sessionSelected.sequenceName || `Session (${userConfiguration.sessionSelected.totalDuration} seconds)`,
+          value: userConfiguration.sessionSelected,
+        }
+      : undefined
+  );
+  const sessionOptions = PRELOADED_SESSIONs.map((session) => ({
+    label: session.sequenceName || `Session (${session.totalDuration} seconds)`,
+    value: session,
+  }));
+
   return (
-    <div className="flex w-full h-full flex-col gap-10 items-enter bg-gray-900  p-3 rounded-md shadow overflow-y-auto">
+    <div className="flex w-full h-full flex-col gap-10 items-enter bg-gray-800  p-3 rounded-md shadow overflow-y-auto">
       <section>
         <header className="w-full flex justify-between items-center border-b border-dotted border-gray-300/40 pb-2 mb-3 font-semibold">
           <h1 className="color-white text-gray-100 ">Time stretch</h1>
@@ -67,18 +82,31 @@ const ConfigurationPanel: React.FC<ConfigurationPanelProps> = ({
           {timeStrechs.map((timeStretch) => (
             <button
               key={timeStretch.id}
-              className={`flex w-full justify-center items-center rounded-md border border-transparent px-4 h-10 text-sm font-medium shadow-sm transition-colors duration-200 ease-in-out ${
+              className={`flex w-full justify-center items-center rounded-md border  px-4 h-10 text-sm font-medium shadow-sm transition-colors duration-200 ease-in-out  ${
                 userConfiguration?.timeStretchSelected?.id === timeStretch.id
-                  ? 'bg-gray-300 text-gray-900 '
-                  : 'cursor-pointer bg-gray-800 text-gray-300 hover:bg-gray-700 hover:text-gray-100'
+                  ? 'bg-gray-300 text-gray-900 border-gray-300'
+                  : 'cursor-pointer bg-gray-900 text-gray-300 hover:bg-gray-700 hover:text-gray-100 border-gray-300/20'
               }`}
               onClick={() => onChangeTimeStretch(timeStretch.id)}
             >
               {timeStretch.label}
             </button>
           ))}
-          <h2 className="color-white text-gray-100 mt-3 pb-2 mb-3 font-normal">or use a session</h2>
-          <ListSelector selectedItem={selectedSessionItem} items={sessionItems} onChange={onChangeSession}/>
+          <CreatableSelectField
+            label="Or select a session"
+            labelClassName="text-gray-100 text-sm"
+            options={sessionOptions}
+            selectedOption={userConfiguration?.sessionSelected ? {
+              label: userConfiguration.sessionSelected.sequenceName || `Session (${userConfiguration.sessionSelected.totalDuration} seconds)`,
+              value: userConfiguration.sessionSelected,
+            } : undefined}
+            onChange={(option) => {
+              setSelectedSessionOption(option || undefined);
+              onChangeSession(option?.value as Session);
+            }}
+            isClearable={false}
+            placeholder='Select session...'
+          />
         </main>
       </section>
       <section>
