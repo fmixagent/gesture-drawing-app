b36fbe5bab3dcf83572616bc5030888eda9e1ffc
Daniel de fuenmayor <fmixagent@gmail.com>
2026-08-29 10:41:01 +0700

    files management separation electron-vite vs vite-react

--- src/renderer/src/service/fs-service.ts
+++ src/renderer/src/service/fs-service.ts
@@ -1,36 +1,7 @@
-import { ImageData } from '@renderer/models/imageData';
+import { environment } from '@renderer/environments/environment';
+import fsServiceElectron from './fs-service.electron';
+import fsServiceWeb from './fs-service.web';
 
-async function getFilesFromDir(folderPath: string, images: ImageData[] = []): Promise<ImageData[]> {
-  const fileList = await window.api.readDirFileNames(folderPath);
-  for (const file of fileList) {
-    const path = `${folderPath}\\${file}`;
-    const imageData: ImageData = {
-      name: file,
-      url: undefined,
-      localPath: `${folderPath}\\${file}`,
-    };
-    if (window.api.isDirectory(path)) {
-      // Recover image
-      await getFilesFromDir(path, images);
-    } else {
-      // Recover the
-      images.push(imageData);
-    }
-  }
-  return images;
-}
-
-const selectFolder = async (): Promise<string | null> => {
-  const folderPath = await window.api.selectDirectory();
-  if (!folderPath) {
-    return null;
-  }
-  return folderPath;
-};
-
-const fsService = {
-  getFilesFromDir,
-  selectFolder,
-};
+const fsService = environment.webVersion ? fsServiceWeb : fsServiceElectron;
 
 export default fsService;
